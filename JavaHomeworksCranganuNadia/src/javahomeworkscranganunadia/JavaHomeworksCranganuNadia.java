39a16489d90b58ee4123156d4c5301391d1649e8
Nadia <Nadia@Nadia-PC>
2017-02-10 22:52:04 +0200

    Homework 1 done!

--- JavaHomeworksCranganuNadia/src/javahomeworkscranganunadia/JavaHomeworksCranganuNadia.java
+++ JavaHomeworksCranganuNadia/src/javahomeworkscranganunadia/JavaHomeworksCranganuNadia.java
@@ -1,21 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * First self made program
  */
 package javahomeworkscranganunadia;
 
+import java.util.Scanner;
 /**
- *
  * @author Nadia
  */
 public class JavaHomeworksCranganuNadia {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+        {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Please enter your user name : ");
+        String userName = input.next();
+        System.out.println ("Your user name is: " + userName);
     }
-    
 }
